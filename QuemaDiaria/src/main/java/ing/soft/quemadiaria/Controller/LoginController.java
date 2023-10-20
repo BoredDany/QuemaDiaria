c8b6247111b26fa164ec5d336bd04c7d685dd200
BoredDany <141600976+BoredDany@users.noreply.github.com>
2023-10-19 22:41:50 -0500

    Delete QuemaDiaria directory

--- QuemaDiaria/src/main/java/ing/soft/quemadiaria/Controller/LoginController.java
+++
@@ -1,4 +0,0 @@
-package ing.soft.quemadiaria.Controller;
-
-public class LoginController {
-}
--- QuemaDiaria/src/main/java/ing/soft/quemadiaria/Model/Domain/Exceptions/TrainerException.java
+++
@@ -1,9 +0,0 @@
-package ing.soft.quemadiaria.Model.Domain.Exceptions;
-
-import ing.soft.quemadiaria.Model.Domain.Entities.Trainer;
-
-public class TrainerException extends Exception{
-    public TrainerException(String message){
-        super(message);
-    }
-}
