5baf5668eaf50dea752f90fcefc2912daa14b08d
BoredDany <141600976+BoredDany@users.noreply.github.com>
2023-10-20 17:03:22 -0500

    Delete QuemaDiaria-Project directory

--- QuemaDiaria-Project/src/main/java/ing/soft/quemadiariaproject/Model/Domain/Exceptions/TrainerException.java
+++
@@ -1,9 +0,0 @@
-package ing.soft.quemadiariaproject.Model.Domain.Exceptions;
-
-import ing.soft.quemadiariaproject.Model.Domain.Entities.Trainer;
-
-public class TrainerException extends Exception{
-    public TrainerException(String message){
-        super(message);
-    }
-}
