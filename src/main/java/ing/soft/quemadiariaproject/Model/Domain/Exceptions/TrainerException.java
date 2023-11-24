4061ba77c687ebfdd9791b59fa8da5416f4160a0
BoredDany <141600976+BoredDany@users.noreply.github.com>
2023-11-24 15:00:12 -0800

    src directory

--- src/main/java/ing/soft/quemadiariaproject/Model/Domain/Exceptions/TrainerException.java
+++
@@ -1,7 +0,0 @@
-package ing.soft.quemadiariaproject.Model.Domain.Exceptions;
-
-public class TrainerException extends Exception{
-    public TrainerException(String message){
-        super(message);
-    }
-}
--- src/main/java/ing/soft/quemadiariaproject/Model/Facade/ProgramFacade.java
+++
@@ -1,7 +0,0 @@
-package ing.soft.quemadiariaproject.Model.Facade;
-
-import java.util.List;
-
-public interface ProgramFacade {
-    List<String> programsResume(String username);
-}
